2ff2d2ac318e2e07c2c038d0c419d1b4e19771ab
Milos <milos.mitrovicbgnp@gmail.com>
2020-09-25 07:28:33 +0200

    Fix test, update yaml file

--- src/BusinessManagerApiTests/Controllers/ClientControllerTests.cs
+++ src/BusinessManagerApiTests/Controllers/ClientControllerTests.cs
@@ -190,7 +190,7 @@ namespace BusinessManagerApiTests.Controllers
             var result = await controller.UpdateClient(clientId, client);
 
             //Assert
-            Assert.IsType<OkObjectResult>(result);
+            Assert.IsType<NoContentResult>(result);
 
             Dispose();
         }
